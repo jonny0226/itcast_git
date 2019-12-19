a038fc7126e1247f278e9cc1e765bd9446cd1514
jonny <zhangjialu0226@163.com>
2019-12-19 18:00:19 +0800

    firstcommit

---
+++ src/main/java/pojo/User.java
@@ -0,0 +1,40 @@
+package pojo;
+
+public class User {
+    private String name;
+    private String sex;
+    private String email;
+
+    public User() {
+    }
+
+    public User(String name, String sex, String email) {
+        this.name = name;
+        this.sex = sex;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

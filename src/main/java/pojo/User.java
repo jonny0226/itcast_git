1b4db9c6250b2c4ddc00b7c717dbd0f8bc3d0bb7
jonny <zhangjialu0226@163.com>
2019-12-19 18:34:51 +0800

    修改User

--- src/main/java/pojo/User.java
+++ src/main/java/pojo/User.java
@@ -37,4 +37,13 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
